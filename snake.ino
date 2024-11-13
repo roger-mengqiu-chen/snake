ecf7d5585abf8ff373233231d17ae0550ba6069d
Roger Chen <rogermchen.ubc@gmail.com>
2024-11-12 21:15:20 -0700

    reformat

--- snake.ino
+++ snake.ino
@@ -16,22 +16,22 @@ int16_t start_x = 0;
 int16_t start_y = 0;
 
 void setup(void) {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // using a 1.44" TFT:
-  tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
-  tft.setRotation(3);
-  tft.fillScreen(0);
-  start_x = tft.width() / 2;
-  start_y = tft.height() / 2;
-  tft.fillRect(start_x, start_y, SNAKE_SIZE, SNAKE_SIZE, 0xffff);
-  delay(4000);
+    // using a 1.44" TFT:
+    tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
+    tft.setRotation(3);
+    tft.fillScreen(0);
+    start_x = tft.width() / 2;
+    start_y = tft.height() / 2;
+    tft.fillRect(start_x, start_y, SNAKE_SIZE, SNAKE_SIZE, 0xffff);
+    delay(4000);
 
 }
 
 void loop() {
-  tft.fillRect(start_x + STEP, start_y, STEP, SNAKE_SIZE, 0x0);
-  start_x -= STEP;
-  tft.fillRect(start_x, start_y, SNAKE_SIZE, SNAKE_SIZE, 0xffff);
-  delay(500);
+    tft.fillRect(start_x + STEP, start_y, STEP, SNAKE_SIZE, 0x0);
+    start_x -= STEP;
+    tft.fillRect(start_x, start_y, SNAKE_SIZE, SNAKE_SIZE, 0xffff);
+    delay(500);
 }
