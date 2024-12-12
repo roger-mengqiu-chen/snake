ba209e76c88eb9720371f171db694d0102d88f40
Roger Chen <rogermchen.ubc@gmail.com>
2024-12-11 22:08:29 -0700

    added some debug info and fixed tail starting location

--- game_elements.cpp
+++ game_elements.cpp
@@ -15,16 +15,20 @@ Snake::Snake(Adafruit_ST7735 *tft)
     Serial.println("Snake constructor");
     m_tft = tft;
     head = Location(m_tft->width() / 2, m_tft->height() / 2, 0);
-    tail = Location(m_tft->width() / 2, m_tft->height() / 2, 0);
+    tail = Location(m_tft->width() / 2 + SNAKE_SIZE, m_tft->height() / 2, 0);
     len = 1;
     head_dir = 0;
     tail_dir = 0;
     queue = new Queue();
     tft->fillRect(head.get_x(), head.get_y(), SNAKE_SIZE, SNAKE_SIZE, SNAKE_COLOR);
-    Serial.print("initialized: ");
+    Serial.print("head starts at: ");
     Serial.print(head.get_x());
     Serial.print(" ");
     Serial.println(head.get_y());
+    Serial.print("tail starts at: ");
+    Serial.print(tail.get_x());
+    Serial.print(" ");
+    Serial.println(tail.get_y());
 }
 
 Snake::Snake(const Snake &s)
@@ -66,6 +70,10 @@ void Snake::turn(uint8_t direction)
     Serial.println("turn");
     head_dir = direction;
     Location turn_pt = Location(head.get_x(), head.get_y(), direction);
+    if (queue->empty()) 
+    {
+        tail_dir = direction;
+    }
     queue->push(turn_pt);
 }
 
@@ -90,5 +98,33 @@ void Snake::h_move()
     {
         head.set_y(head.get_y() + STEP);
         m_tft->fillRect(head.get_x(), head.get_y(), SNAKE_SIZE, SNAKE_SIZE, SNAKE_COLOR);
+    }
+}
+
+void Snake::t_move() 
+{
+    Location* next_location = queue->top();
+    if (nullptr != next_location && next_location->get_x() == tail.get_x() && next_location->get_x() == tail.get_x())
+    {
+        Serial.print("next location: ");
+        Serial.print(next_location->get_x());
+        Serial.print(" ");
+        Serial.print(next_location->get_y());
+        Serial.print(" ");
+        tail_dir = next_location->get_z();
+        Serial.print(tail_dir);
+        queue->pop();
+    }
+
+    if (tail_dir == LEFT)
+    {
+        Serial.println("tail moving left");
+        tail.set_x(tail.get_x() - STEP);
+        Serial.print("tail x: ");
+        Serial.print(tail.get_x());
+        Serial.print(" tail y: ");
+        Serial.print(tail.get_y());
+        Serial.println();
+        m_tft->fillRect(tail.get_x(), tail.get_y(), STEP, SNAKE_SIZE, BACKGROUND);
     }
 }
